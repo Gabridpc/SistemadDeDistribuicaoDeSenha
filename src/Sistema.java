994cbdf977ada41afdce8f220743e06851617299
MatheusCequeira <escrevaparamatheus@gmail.com>
2018-07-03 17:42:29 -0300

    Criada função para ajustar formato de senha ao monitor

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,16 +1,34 @@
 
 public class Sistema {
+	
+	private static String nomeiaSenha(String prior, int num) {
+		String sSenha = "";
+		if(num<1000) {
+			sSenha = "0";
+			if(num<100) {
+				sSenha = sSenha + "0";
+				if(num<10) {
+					sSenha = sSenha + "0";
+				}
+			}
+		}
+		sSenha = prior + sSenha + Integer.toString(num);
+		return sSenha;
+	}
 
 	public static void main(String[] args) {
 	
 		String prior;
-		prior = "c";
+		prior = "A";
 		
 		int num;
-		num = 0001;
+		num = 1;
 		
 		Senha senha1 = new Senha(prior, num);
-		System.out.println("Sua senha: "+senha1.getPrioridade()+senha1.getNumero());
+				
+		System.out.println("Sua senha : "+Sistema.nomeiaSenha(senha1.getPrioridade(),senha1.getNumero()));
+		
+		System.out.println("Sua senha (Aritmética): "+senha1.getPrioridade()+senha1.getNumero());
 
 	}
 
