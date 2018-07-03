e2875d098987905e1a64fe666f0ad9f80e442ffe
MatheusCequeira <escrevaparamatheus@gmail.com>
2018-07-03 00:08:03 -0300

    classe senha com atributos, getters e setters criados

--- src/Senha.java
+++ src/Senha.java
@@ -1,4 +1,20 @@
-
 public class Senha {
 
+	private char prioridade;
+	private int numero;
+	
+	public char getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(char prioridade) {
+		this.prioridade = prioridade;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+		
 }
